88e76afb9fc289d7d2f866316d45f10eb6078077
misterchangray@hotmail.com <misterchangray@hotmail.com>
2021-10-26 16:55:39 +0800

    feat: add local redis configuration

--- faster-financial/src/main/java/com/github/misterchangray/financial/service/impl/CacheServiceImpl.java
+++ faster-financial/src/main/java/com/github/misterchangray/financial/service/impl/CacheServiceImpl.java
@@ -2,12 +2,18 @@ package com.github.misterchangray.financial.service.impl;
 
 import com.github.misterchangray.financial.service.CacheService;
 import com.github.misterchangray.financial.v001.mapper.po.FinancialAccount;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CacheServiceImpl implements CacheService {
+    @Autowired()
+    RedissonClient redissonClient;
+
     @Override
     public int save(FinancialAccount financialAccount) {
         return 0;
@@ -23,6 +29,7 @@ public class CacheServiceImpl implements CacheService {
         return 0;
     }
 
+
     @Override
     public FinancialAccount getAccount(String id) {
         return null;
@@ -31,5 +38,9 @@ public class CacheServiceImpl implements CacheService {
     @Override
     public List<FinancialAccount> getAccount(String... ids) {
         return null;
+    }
+
+    public boolean enableRedis () {
+        return Objects.nonNull(redissonClient);
     }
 }
--- faster-framework-redis/readme.md
+++ faster-framework-redis/readme.md
@@ -1,5 +1,6 @@
 # Redisson 框架整合包
 
+[更多使用示例参考: Redisson 使用手册](https://www.bookstack.cn/read/redisson-wiki-zh/spilt.1.6.-%E5%88%86%E5%B8%83%E5%BC%8F%E5%AF%B9%E8%B1%A1.md)
 
 ##### 1. 引入maven
 ```xml
@@ -26,3 +27,23 @@ com:
 ```
 
 ##### 3. 可以在项目中注入使用`RedissonClient`来进行开发了
+```java
+
+    @Autowired(required = false)
+    RedissonClient redissonClient;
+
+    @Override
+    public int save(FinancialAccount financialAccount) {
+        // 构建Key对象
+        RBucket<Object> bucket = redissonClient.getBucket("com.github.misterchangray.faster.user");
+        // 尝试设置, 未设置则设置成功
+        boolean b = bucket.trySet("{}", 22, TimeUnit.SECONDS);
+        // 获取后删除
+        Object andDelete = bucket.getAndDelete();  
+        // 获取值
+        Object o = bucket.get();
+
+    }
+
+```
+
