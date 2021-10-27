9a4a71650d5f498393d9c6914c9907f8573b6802
misterchangray@hotmail.com <misterchangray@hotmail.com>
2021-10-27 13:41:33 +0800

    feat: add some enums for normal use

--- faster-framework-common/src/main/java/com/github/misterchangray/common/base/BaseEnum.java
+++ faster-framework-common/src/main/java/com/github/misterchangray/common/base/BaseEnum.java
@@ -2,7 +2,14 @@ package com.github.misterchangray.common.base;
 
 public enum BaseEnum implements ResEnum {
     SUCCESS("0000", "SUCCESS"),
-    FAIL("0000", "FAIL"),
+    FAIL("9999", "FAIL"),
+
+    INVALID_REQUEST("0001", "INVALID_REQUEST"),
+    INVALID_PARAM("0002", "INVALID_PARAM"),
+    INVALID_USER("0003", "INVALID_USER"),
+    INVALID_SESSION("0004", "INVALID_SESSION"),
+    INVALID_ACCOUNT("0005", "INVALID_ACCOUNT"),
+
     ;
 
     private String code;
